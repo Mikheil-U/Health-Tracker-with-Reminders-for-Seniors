356f39a988211dd22d89db1f850c459cfc1afcac
lashakal <111999712+lashakal@users.noreply.github.com>
2023-11-24 21:59:00 -0500

    added initial code for GUI

---
+++ main.py
@@ -0,0 +1,159 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+from tkinter import messagebox
+from tksheet import Sheet
+
+# Function to handle login window
+def login_window():
+    login_window = tk.Tk()
+    login_window.title("Log In")
+    login_window.geometry("800x500")
+
+    # Variables to store user input
+    username_var = tk.StringVar()
+    password_var = tk.StringVar()
+
+    # Function to handle login button click
+    def login():
+        # TODO - update the login requirements
+        username = username_var.get()
+        password = password_var.get()
+
+        # TODO - login
+        # Check if the username and password match
+        if username == "user" and password == "hey":
+            login_window.destroy()
+            main_window()
+            return
+
+        # Display an error message if login fails
+        messagebox.showerror("Error", "Invalid username or password")
+
+    # Create widgets for login window
+    tk.Label(login_window, text="WELCOME TO YOUR HEALTH TRACKER!", font=("Arial 26 bold")).pack(pady=40)
+    tk.Label(login_window, text="Please log in to your account", font=("Arial 18 bold")).pack(pady=10)
+    tk.Label(login_window, text="Username:").pack()
+    tk.Entry(login_window, textvariable=username_var).pack()
+    tk.Label(login_window, text="Password:").pack()
+    tk.Entry(login_window, textvariable=password_var, show="*").pack()
+    tk.Button(login_window, text="Log In", command=login).pack(pady=10)
+    tk.Button(login_window, text="Sign Up", command=signup_window).pack()
+
+    login_window.mainloop()
+
+
+# Function to handle sign-up window
+def signup_window():
+    # signup_window = tk.Toplevel(root)
+    signup_window = tk.Tk()
+    signup_window.title("Sign Up")
+
+    # Variables to store user input
+    username_var = tk.StringVar()
+    password_var = tk.StringVar()
+
+    # Function to handle sign-up button click
+    def signup():
+        # TODO - update the fields according to login requirements
+        username = username_var.get()
+        password = password_var.get()
+
+        # TODO - Store user information
+
+        # Close sign-up window
+        signup_window.destroy()
+
+        messagebox.showinfo("Sign Up", "Singing up successful")
+
+    # Create widgets for sign-up window
+    tk.Label(signup_window, text="Username:").pack()
+    tk.Entry(signup_window, textvariable=username_var).pack()
+    tk.Label(signup_window, text="Password:").pack()
+    tk.Entry(signup_window, textvariable=password_var, show="*").pack()
+    tk.Button(signup_window, text="Sign Up", command=signup).pack()
+
+
+# Function to handle the main window
+def main_window():
+    # main_window = tk.Toplevel(root)
+    main_window = tk.Tk()
+    main_window.title("Health Tracker")
+    main_window.geometry("800x500")
+
+    # Create notebook with three frames
+    notebook = ttk.Notebook(main_window)
+
+    # Health Information Page
+    health_frame = tk.Frame(notebook)
+    notebook.add(health_frame, text='Health Info')
+    create_health_page(health_frame)
+
+    # Medications Page
+    medications_frame = tk.Frame(notebook)
+    notebook.add(medications_frame, text='Medications')
+    create_medications_page(medications_frame)
+
+    # Appointments Page
+    appointments_frame = tk.Frame(notebook)
+    notebook.add(appointments_frame, text='Appointments')
+    create_appointments_page(appointments_frame)
+
+    notebook.pack(expand=True, fill="both")
+
+    main_window.mainloop()
+
+
+def create_health_page(frame):
+    # Add widgets for health information page
+    label = tk.Label(frame, text="Enter health information:")
+    label.pack(pady=10)
+
+    # TODO - Add health information of the user and other stuff
+
+def create_medications_page(frame):
+    # Add widgets for medications page
+    label = tk.Label(frame, text="Track your medications and set reminders:")
+    label.pack(pady=10)
+
+    # TODO - store sql table of medications in data list to open with tksheet
+    # now data is only for demonstration purposes
+    data = [
+        ['John', 25, 'New York'],
+        ['Alice', 30, 'Los Angeles'],
+        ['Bob', 22, 'Chicago'],
+    ]
+
+    # adding the medications sheet to the frame
+    medications_sheet = Sheet(frame)
+    medications_sheet.set_sheet_data(data)
+    medications_sheet.enable_bindings(("single_select"))
+    medications_sheet.place(x=25, y=50, width=700, height=300)
+
+    def delete_selected_medication_row():
+        selected_cell = medications_sheet.get_currently_selected()
+        selected_row = selected_cell.row
+        print(selected_row)
+        del data[selected_row]
+        # TODO - delete from the sql table
+        medications_sheet.set_sheet_data(data)
+        medications_sheet.refresh()
+
+    # Create a button to delete the selected row
+    delete_button = tk.Button(frame, text="Delete Selected Row", fg="red", command=delete_selected_medication_row)
+    delete_button.pack(side=BOTTOM)
+
+    # TODO - add functionality to add_medication_button
+    add_medication_button = tk.Button(frame, text="Add new medication")
+    add_medication_button.pack(side=BOTTOM)
+
+def create_appointments_page(frame):
+    # Add widgets for appointments page
+    label = tk.Label(frame, text="Manage your doctor appointments:")
+    label.pack()
+
+    # TODO - copy-paste and adjust whatever we have in create_medications_page
+
+
+# Open the login window to start the program
+login_window()
